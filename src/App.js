6845e9ed29091e296c36cc2fe4a78a5ecedeb4bc
Yura-student <yuriy.storozhev@mail.ru>
2022-07-08 00:21:34 +0700

    Добавлен массив

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import List from "./Tz/List"
+import Years from "./Tz/Years";
+import Books from "./Tz/Books";
 
 function App() {
+
+  let years = [
+    {id:1, completed: false, title: '2020'},
+    {id:2, completed: false, title: '2019'},
+    {id:3, completed: false, title: '2018'}
+  ]
+
   return (
     <div className="wrapper">
+    <h1>Рекомендуемая книга</h1>
 
-    <List />
+    <List years={years} />
     </div>
   )
 }
